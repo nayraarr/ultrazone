be04fdfd9e5a1887871cb0a60be9f040d41d84a1
nayraarr <zita.nayra.ardini@gmail.com>
2025-09-08 05:19:37 +0700

    Mengonfigurasikan Routing URL

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'name': 'Mens Tiro Vis Tech Competition Jacket Grey/Lime Green',
+        'price': '90',
+        'desc' : 'Stay sharp on and off the pitch with the Tiro Vis Tech Competition Jacket. \
+            Designed in a sleek grey base with striking lime green accents, this jacket combines sporty style with functional comfort. \
+            Lightweight yet durable, it’s built with breathable fabric to keep you cool during training and stylish enough for casual wear. \
+            Featuring a full-zip front, stand-up collar, and tailored fit, it’s perfect for athletes and fans who want performance and flair in one piece.'
+    }
+
+    return render(request, "main.html", context)
