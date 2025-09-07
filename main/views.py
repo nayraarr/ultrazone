0f4c333f11e6d85fb17c492ce55447939f239e53
nayraarr <zita.nayra.ardini@gmail.com>
2025-09-08 05:25:12 +0700

    Mengubah isi template html

--- main/views.py
+++ main/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Mens Tiro Vis Tech Competition Jacket Grey/Lime Green',
-        'price': '90',
-        'desc' : 'Stay sharp on and off the pitch with the Tiro Vis Tech Competition Jacket. \
-            Designed in a sleek grey base with striking lime green accents, this jacket combines sporty style with functional comfort. \
-            Lightweight yet durable, it’s built with breathable fabric to keep you cool during training and stylish enough for casual wear. \
-            Featuring a full-zip front, stand-up collar, and tailored fit, it’s perfect for athletes and fans who want performance and flair in one piece.'
+        'npm': '2406404913',
+        'name': 'Zita Nayra Ardini',
+        'class' : 'PBP F'
     }
 
     return render(request, "main.html", context)
