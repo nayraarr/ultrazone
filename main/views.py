c1e50e570f2bc6019ef71c0be8d658aedcb4ecd8
nayraarr <zita.nayra.ardini@gmail.com>
2025-09-21 21:17:22 +0700

    Mengimplementasikan FUngsi Registrasi, Login, dan Logout

--- main/views.py
+++ main/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.core import serializers
 from main.models import Product
 from main.forms import ProductForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def show_main(request):
     product_list = Product.objects.all()
@@ -59,3 +63,39 @@ def show_json_by_id(request, product_id):
         return HttpResponse(json_data, content_type="application/json")
     except Product.DoesNotExist:
         return HttpResponse(status=404)
+    
+def register(request):
+    form = UserCreationForm()
+    
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created')
+            return redirect('main:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        
+        if form.is_valid():
+            user = form.get_user
+            login(request, user)
+            return redirect('main:show_main')
+    
+    else:
+        form = AuthenticationForm(request)
+    
+    context = {'form':form}
+    return render(request, 'login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('main:login')
+
+
+
