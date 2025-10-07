4456ad513c087bba08a3edfe6bb1654765304933
nayraarr <zita.nayra.ardini@gmail.com>
2025-10-07 22:23:01 +0700

    Perbaikan Models Product

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,4 @@ from main.models import Product
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'price', 'description', 'thumbnail', 'category', 'is_featured', 'brand']
+        fields = ['name', 'price', 'description', 'thumbnail', 'rating','category', 'is_discount', 'brand']
--- main/models.py
+++ main/models.py
@@ -14,14 +14,14 @@ class Product(models.Model):
         ('accessories', 'Accessories'),
     ]
     
-    # nama, harga, deskripsi, thumbnail, kategori, is_featured, brand, rating
+    # nama, harga, deskripsi, thumbnail, kategori, is_discount, brand, rating
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='accessories')
-    is_featured = models.BooleanField(default=False)
+    is_discount = models.BooleanField(default=False)
     brand = models.CharField(max_length=20)
     rating = models.FloatField(default=0.0)
 
